81349e3a0e30193c59ac8d709db3af1ba74ea3c7
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-09-28 19:59:25 +0300

    Setup tableView methods

--- Milestone-Projects 19-21/ViewController.swift
+++ Milestone-Projects 19-21/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+var notes = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Notes"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNote))
+        navigationItem.rightBarButtonItem?.tintColor = .systemOrange
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Note", for: indexPath)
+        for (index,word) in notes[indexPath.row].enumerated() {
+            if index < 5 {
+                cell.textLabel?.text?.append(word)
+            }
+        }
+        return cell
+    }
+    
+    @objc func addNote() {
+        
+      
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+        
+        
+    }
+    
 
+    
 }
 
